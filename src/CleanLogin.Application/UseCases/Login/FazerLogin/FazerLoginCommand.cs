f7aeb7c4da8a9550d806ee3406493edf9beac3d1
Diego Doná <diedona@gmail.com>
2021-06-26 11:37:58 -0300

    criando o application

---
+++ src/CleanLogin.Application/UseCases/Login/FazerLogin/FazerLoginCommand.cs
@@ -0,0 +1,16 @@
+﻿using CleanLogin.Domain.DTOs.Usuario;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanLogin.Application.UseCases.Login.FazerLogin
+{
+    public class FazerLoginCommand : IRequest<UsuarioDTO>
+    {
+        public string NomeDeUsuario { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ src/CleanLogin.Application/UseCases/Login/FazerLogin/FazerLoginHandler.cs
@@ -0,0 +1,24 @@
+﻿using CleanLogin.Domain.DTOs.Usuario;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanLogin.Application.UseCases.Login.FazerLogin
+{
+    public class FazerLoginHandler : IRequestHandler<FazerLoginCommand, UsuarioDTO>
+    {
+        public async Task<UsuarioDTO> Handle(FazerLoginCommand request, CancellationToken cancellationToken)
+        {
+            return new UsuarioDTO()
+            {
+                Id = Guid.NewGuid(),
+                Nome = request.NomeDeUsuario,
+                Sobrenome = "Sobrenome Fixo"
+            };
+        }
+    }
+}
