884eb79ba7ee4a69854c52fdd1bf6f7fba0a6ea8
Diego Doná <diedona@gmail.com>
2021-06-26 19:26:23 -0300

    iniciando login

---
+++ src/CleanLogin.Infra/Login/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanLogin.Infra.Login
+{
+    public static class Settings
+    {
+        public static string Secret => "amor#nanana$alada18";
+
+        public static DateTime ExpireTime => DateTime.Now.AddHours(8);
+    }
+}
---
+++ src/CleanLogin.Infra/Login/TokenService.cs
@@ -0,0 +1,29 @@
+﻿using CleanLogin.Application.UseCases.Login.FazerLogin;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CleanLogin.Infra.Login
+{
+    public class TokenService
+    {
+        public string GerarToken(FazerLoginCommand fazerLogin)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, fazerLogin.NomeDeUsuario)
+                }),
+                Expires = Settings.ExpireTime,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
