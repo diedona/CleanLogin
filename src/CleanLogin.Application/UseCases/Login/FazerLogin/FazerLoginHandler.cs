4742549522c3d90c243eef12ad36a4b135b08703
Diego Doná <diedona@gmail.com>
2021-06-27 14:39:59 -0300

    configurando conexão com banco

--- src/CleanLogin.Application/UseCases/Login/FazerLogin/FazerLoginHandler.cs
+++ src/CleanLogin.Application/UseCases/Login/FazerLogin/FazerLoginHandler.cs
@@ -1,4 +1,5 @@
 ﻿using CleanLogin.Application.Interfaces.Services;
+using CleanLogin.Application.Interfaces.UnitOfWork;
 using CleanLogin.Domain.DTOs.Usuario;
 using MediatR;
 using System;
@@ -13,20 +14,26 @@ namespace CleanLogin.Application.UseCases.Login.FazerLogin
     public class FazerLoginHandler : IRequestHandler<FazerLoginCommand, UsuarioDTO>
     {
         private readonly ITokenService _TokenService;
+        private readonly IUnitOfWork _UnitOfWork;
 
-        public FazerLoginHandler(ITokenService tokenService)
+        public FazerLoginHandler(ITokenService tokenService, IUnitOfWork unitOfWork)
         {
             this._TokenService = tokenService;
+            _UnitOfWork = unitOfWork;
         }
 
         public async Task<UsuarioDTO> Handle(FazerLoginCommand request, CancellationToken cancellationToken)
         {
-            if (!request.NomeDeUsuario.Equals("adm") && !request.Senha.Equals("123"))
+            //if (!request.NomeDeUsuario.Equals("adm") && !request.Senha.Equals("123"))
+            //    return null;
+
+            var usuario = _UnitOfWork.UsuarioRepository.CarregarPorNomeDeUsuario(request.NomeDeUsuario);
+            if (usuario == null)
                 return null;
 
             return new UsuarioDTO()
             {
-                Id = Guid.NewGuid(),
+                Id = usuario.Id,
                 Token = _TokenService.GerarToken(request)
             };
         }
